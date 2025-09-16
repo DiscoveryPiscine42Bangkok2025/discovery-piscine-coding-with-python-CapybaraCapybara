8fa39d076e9f25f222cf68c332068428ec3c023f
Pleum <66070263@gmail.com>
2025-09-16 15:47:28 +0700

    cell03\ex00 to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+start = int(input("Enter a number less than 25: \n"))
+if (start > 25):
+    print("Error")
+else:
+    while(start <= 25):
+        print(f"Inside the loop, my variable is {start}")
+        start+=1
