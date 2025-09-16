a3c8c2afc5a94428d32c5ace5fb627f635aab63f
Pleum <66070263@gmail.com>
2025-09-16 16:44:44 +0700

    cell03\ex03 edit

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,9 +1,9 @@
 out = 0
-while(out < 10):
+while(out < 11):
     ins = 0
     print(f"Table de {out}: ", end="")
     inn = 0
-    while inn < 10:
+    while inn < 11:
         print(out * inn, end=" ")
         inn+=1
     print()
