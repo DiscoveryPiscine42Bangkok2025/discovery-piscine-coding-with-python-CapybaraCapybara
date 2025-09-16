608121efcc8756f19f198f0670716758649d578a
Pleum <66070263@gmail.com>
2025-09-16 15:42:43 +0700

    cell\ex03 edit "Add else and edit method"

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -2,7 +2,9 @@ first = int(input("Enter the first number:"))
 sec = int(input("Enter the second number:"))
 sum = first*sec
 print(f'{first} x {sec} = {sum}')
-if (sum >= 0):
+if (sum > 0):
     print("The result is positive.")
+elif (sum == 0):
+    print("The result is positive and negative.")
 else:
     print("The result is negative.")
