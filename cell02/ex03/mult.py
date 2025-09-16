d3e17721ba881e0507f5de22d969eb0b2319c30f
Pleum <66070263@gmail.com>
2025-09-16 15:38:09 +0700

    cell02\ex03 mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,8 @@
+first = int(input("Enter the first number:"))
+sec = int(input("Enter the second number:"))
+sum = first*sec
+print(f'{first} x {sec} = {sum}')
+if (sum >= 0):
+    print("The result is positive.")
+else:
+    print("The result is negative.")
