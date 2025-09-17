40f9d1e200a409ed86f02a2d5a4414a11837b93a
Pleum <66070263@gmail.com>
2025-09-17 16:11:44 +0700

    cell04\ex01 age.py

---
+++ cell04/ex01/age.py
@@ -0,0 +1,9 @@
+nage = int(input("Please tell me your age: "))
+print(f"You are currently {nage} years old.")
+i = 0
+j = 10
+while(i<3):
+    print(f"In {j} years, you'll be {nage+j} years old.")
+    j+=10
+    i+=1
+
